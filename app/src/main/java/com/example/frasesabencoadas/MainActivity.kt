16b45817c8471dd94cffe77947284a2e43c50a81
Thiago Torres <137134010+ThiagoTorresFerrao@users.noreply.github.com>
2026-02-09 23:55:10 -0300

    Primeiro commit - app Android inicial para ensino em sala de aula onde leciono o curso de Análise e Desenvolvimento de Software

---
+++ app/src/main/java/com/example/frasesabencoadas/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.frasesabencoadas
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.frasesabencoadas.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val lista = resources.getStringArray(R.array.frases)
+
+        binding.btnGerador.setOnClickListener {
+            val indice = Random.nextInt(lista.size)
+        // disparado depois do touch
+        binding.txtFrase.text = lista[indice]
+        }
+    }
+}
